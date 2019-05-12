a02d9e2a690c33188b6987e69ee9785c458d0388
Blake Rose <bdecktor@gmail.com>
2019-05-12 21:11:51 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -22,8 +22,8 @@ def word_substituter(tweet)
   # replaces long words with their expected short form
 
     tweet.split(" ").map do |word|
-      if dictionary.keys.include?(word.downcase)
-        word = dictionary[word.downcase]
+      if dictionary.keys.include?(word)
+        word = dictionary[word]
       else
         word
     end
